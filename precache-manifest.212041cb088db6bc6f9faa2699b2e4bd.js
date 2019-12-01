b0b30ba976f1e3cc9a98a3caf7cddd9c0404f09a
Joshua dela Cruz <joshua@ecommercewebsites.com.au>
2019-12-01 20:02:06 +0800

    Updates

---
+++ precache-manifest.212041cb088db6bc6f9faa2699b2e4bd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9118260f037ae4414235b863f812c708",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "01e5cb1c971a9b971060",
+    "url": "/monster-rolodex/static/css/main.4328b897.chunk.css"
+  },
+  {
+    "revision": "7fd0257753c13451b492",
+    "url": "/monster-rolodex/static/js/2.837c49c4.chunk.js"
+  },
+  {
+    "revision": "01e5cb1c971a9b971060",
+    "url": "/monster-rolodex/static/js/main.03950787.chunk.js"
+  },
+  {
+    "revision": "2539a77c6447b7271c9d",
+    "url": "/monster-rolodex/static/js/runtime-main.3c6f88f8.js"
+  }
+]);
